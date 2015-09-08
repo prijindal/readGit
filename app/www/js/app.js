9bb5a193aeee22b0715841de8c4c025c76df52c9
prijindal <priyanshujindal1995@gmail.com>
2015-09-08 12:23:11 +0530

    Basic Login and newsfeed completed

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,5 +1,10 @@
 
-angular.module('readGit', ['ngRoute', 'ngAnimate'])
+angular.module('readGit', [
+        'ngRoute',
+        'ngAnimate',
+        'ngStorage',
+        'yaru22.angular-timeago'
+    ])
         .config(["$routeProvider", function($routeProvider) {
             $routeProvider
             .when('/login', {
@@ -15,19 +20,66 @@ angular.module('readGit', ['ngRoute', 'ngAnimate'])
 
             $routeProvider.otherwise('/login')
         }])
+        .run(['$rootScope', '$location', 'credentials', function ($rootScope, $location, credentials) {
+            $rootScope.$on('$routeChangeStart', function (event) {
+                if (!credentials.isLoggedIn()) {
+                    console.log('DENY');
+                    //event.preventDefault();
+                    $location.path('/login');
+                }
+                else {
+                    if($location.path() == '/login') {
+                        $location.replace();
+                      $location.path('/home');
+                    }
+                }
+            });
+        }]);
 
 
 
-angular.module('readGit').controller('loginController',['$location', function($location) {
+
+
+angular.module('readGit').controller('loginController',['$location','newsFeed','credentials', function($location,newsFeed, credentials) {
     var self = this;
-    self.submit = function() {
-        console.log(self.user)
+    self.state = false
+    if (credentials.isLoggedIn()) {
+        $location.replace();
         $location.path('/home')
+    }
+    self.submit = function() {
+        self.state = true
+        credentials.save(self.user)
+        newsFeed.saveData(credentials.get().username, function(data) {
+            $location.replace();
+            $location.path('/home')
+            self.state = false
+        })
     }
     input_fix()
 }])
 
 
-angular.module('readGit').controller('homeController',['$location', function($location) {
+angular.module('readGit').controller('homeController',['$location', 'newsFeed','credentials', function($location, newsFeed, credentials) {
     var self = this;
+
+    var getNewData = function() {
+        console.log('Getting new data')
+        self.loadingNew = true
+        newsFeed.saveData(credentials.get().username, function(data) {
+            self.newsfeed = data
+            self.loadingNew = false
+            console.log('new data loaded')
+        })
+    }
+
+    if(credentials.isLoggedIn()) {
+        console.log(credentials.get());
+        self.newsfeed = newsFeed.getData()
+        getNewData()
+    }
+    else {
+        $location.path('/login')
+    }
+
 }])
