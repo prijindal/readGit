2fdf9eece6198e7b3f89865b73470a083377617d
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-06-07 10:02:29 +0530

    Initialized Ionic Repo

---
+++ app/pages/getting-started/getting-started.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+
+  }
+}
