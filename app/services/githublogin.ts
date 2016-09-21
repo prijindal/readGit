7f5c0787f4780091d1d07fc947741577656bf068
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-21 14:33:26 +0530

    Added Oauth Cordova module

--- app/services/githublogin.ts
+++ app/services/githublogin.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs';
 
-declare var cordova: any;
 
 @Injectable()
 export class GithubLogin {
@@ -22,26 +21,9 @@ export class GithubLogin {
     if (this.loggingIn) return ;
     this.loggingIn = true;
     return new Promise((resolve, reject) => {
-      if (typeof cordova !== 'undefined') {
-        if (cordova.InAppBrowser) {
-          var browserRef = cordova.InAppBrowser.open('https://github.com/login/oauth/authorize?client_id=' + this.clientId + '&redirect_uri=' + this.redirect_uri, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-          browserRef.addEventListener('loadstart', (event: any) => {
-            if ((event.url).indexOf(this.redirect_uri) === 0) {
-              this.requestToken = (event.url).split('code=')[1];
-              this.authenticate()
-              .catch((err) => {
-                reject(err);
-                return Observable.throw(err);
-              })
-              .subscribe(() => {
-                resolve();
-              });
-              browserRef.close();
-            }
-          });
-          browserRef.addEventListener('exit', function(event) {
-            reject('The sign in flow was canceled');
-          });
+      if (typeof window['cordova'] !== 'undefined') {
+        if (window['OAuth']) {
+          this.loginHelper(resolve, reject);
         } else {
           return this.loginWithOauth(resolve, reject);
         }
@@ -55,23 +37,27 @@ export class GithubLogin {
     var script = document.createElement( 'script' );
     script.type = 'text/javascript';
     script.onload = () => {
-      console.log('Script Loaded');
-      let OAuth = window['OAuth'];
-      OAuth.initialize(this.OAuth_KEY);
-      OAuth.popup('github')
-      .done(result => {
-        console.dir(result);
-        this.token = result.access_token;
-        this.updateLocalStorage();
-        resolve();
-      })
-      .fail(err => {
-        console.error(err);
-        reject('The sign in flow was canceled');
-      });
+      this.loginHelper(resolve, reject);
     };
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/oauth-io/0.5.2/oauth.js';
     document.getElementsByTagName('html')[0]['append'](script);
+  }
+
+  loginHelper(resolve, reject) {
+    console.log('Script Loaded');
+    let OAuth = window['OAuth'];
+    OAuth.initialize(this.OAuth_KEY);
+    OAuth.popup('github', {cache: true})
+    .done(result => {
+      console.dir(result);
+      this.token = result.access_token;
+      this.updateLocalStorage();
+      resolve();
+    })
+    .fail(err => {
+      console.error(err);
+      reject('The sign in flow was canceled');
+    });
   }
 
   authenticate() {
