edea4f28fb87379ad17397afd8a64150bca76613
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-22 22:24:35 +0530

    Added Infinite Scroll to notifications page

--- app/pages/notifications-page/notifications-page.ts
+++ app/pages/notifications-page/notifications-page.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectorRef} from '@angular/core';
+import {Component, ChangeDetectorRef, ViewChild} from '@angular/core';
 import {NavController, NavParams, PopoverController} from 'ionic-angular';
 
 import OctokatService from '../../services/octokat';
@@ -7,12 +7,17 @@ import { ErrorPage } from '../error-page/error-page';
 
 import { Popover } from './popover/popover';
 
+const PER_PAGE: number = 10;
+const LIMIT: number = 300;
+
 @Component({
   templateUrl: 'build/pages/notifications-page/notifications-page.html'
 })
 export class NotificationsPage {
+  @ViewChild('notificationsContent') homeContent;
   public loading: Boolean = true;
   public notifications: any = [];
+  private page: number = 1;
 
   constructor(
     private ref: ChangeDetectorRef,
@@ -23,20 +28,47 @@ export class NotificationsPage {
   ) { }
 
   ionViewWillEnter() {
-    this.getNotifications();
+    this.refreshEvents();
   }
 
-  getNotifications() {
+  refreshEvents() {
     this.loading = true;
-    this.octokat.octo.notifications.read()
-    .then(res => {
+    this.page = 1;
+    this.getNotifications(true)
+    .then(() => {
       this.loading = false;
-      this.notifications = JSON.parse(res);
+    });
+  }
+
+  getNotifications(shouldRefresh: Boolean = false) {
+    return this.octokat.octo.fromUrl('/notifications' + '?page=' + this.page + '&per_page=' + PER_PAGE).read()
+    .then(res => {
+      res = JSON.parse(res);
+      if (shouldRefresh) {
+        this.homeContent.scrollTo(0, 0);
+        this.notifications = [];
+      }
+      res.forEach((notification) => {
+        this.notifications.push(notification);
+      });
       this.ref.detectChanges();
+      return res;
     })
     .catch(err => {
       this.nav.push(ErrorPage, {error: err});
     });
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page += 1;
+    if (this.page <= LIMIT / PER_PAGE) {
+      this.getNotifications()
+      .then(() => {
+        infiniteScroll.complete();
+      });
+    } else {
+      infiniteScroll.enable(false);
+    }
   }
 
   openRepository(notification) {
