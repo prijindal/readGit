33cc0d7fcb80907c64b243075136a1a25211db70
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-23 13:38:01 +0530

    Fixed Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-npm run gulp build -- --release --change-host
+npm run gulp build -- --release
 chmod +x hooks/after_prepare/010_add_platform_class.js
-chmod +x hooks/after_prepare/020_replace_text.js
-chmod +x hooks/after_prepare/030_remove_onesync.js
 npm run cordova platform add browser
 npm run cordova build browser -- --release
 gzip -k -r --best --force platforms/browser/www
