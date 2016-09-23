f978ea4cf8af25c66df02e4b5ceabfa3abdc016e
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-23 13:39:21 +0530

    Fixed Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-npm run gulp build -- --release
+npm run build
 chmod +x hooks/after_prepare/010_add_platform_class.js
 npm run cordova platform add browser
 npm run cordova build browser -- --release
