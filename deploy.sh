469a0e9c5365fd41583cfc6fbfcdcb2c2d8cb2e1
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-23 14:04:02 +0530

    Added Github.JS in heroku

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-npm run build
+ioic build --release
+npm run octokat
 # chmod +x hooks/after_prepare/010_add_platform_class.js
 # npm run cordova platform add browser
 # npm run cordova build browser -- --release
---
+++ github.js
@@ -0,0 +1,1 @@
+window.Octokat = require('octokat');
