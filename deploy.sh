9392661b8f603c69d4805a15a5cc27c8f3d9ff78
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-09-23 13:43:07 +0530

    Removed Cordova Scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npm run build
-chmod +x hooks/after_prepare/010_add_platform_class.js
-npm run cordova platform add browser
-npm run cordova build browser -- --release
-gzip -k -r --best --force platforms/browser/www
+# chmod +x hooks/after_prepare/010_add_platform_class.js
+# npm run cordova platform add browser
+# npm run cordova build browser -- --release
+gzip -k -r --best --force www
