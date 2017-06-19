186362a7d44ac575a37b1ee83d5317376099e1dc
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-06-19 19:20:03 +0530

    Added Login Component

---
+++ src/helpers/sleep.js
@@ -0,0 +1,5 @@
+const sleep = (timeout) => new Promise(function(resolve, reject) {
+  setTimeout(resolve, timeout);
+});
+
+export default sleep;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,11 @@
 // @flow
 import React, { Component } from 'react';
-import { Text, Button, AsyncStorage } from 'react-native';
+import { Text, Button, View, AsyncStorage, TextInput } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 
+import { primary } from '../colors';
 import Layout from '../components/Layout/Layout';
+import sleep from '../helpers/sleep';
 
 const navigateToApp = NavigationActions.reset({
   index: 0,
@@ -14,24 +16,73 @@ const navigateToApp = NavigationActions.reset({
   ]
 })
 
+const styles = {
+  textfield: {
+    height: 56,
+    marginHorizontal: 4,
+    paddingHorizontal: 8,
+  },
+  button: {
+    height: 56,
+    marginVertical: 8,
+    paddingHorizontal: 8,
+  }
+}
+
 export default class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    loading: false,
+  }
+  passwordInput: any;
+
   login = async () => {
-    await AsyncStorage.setItem('token', 'token');
-    const user = {user: 'user'}
-    await AsyncStorage.setItem('user', JSON.stringify(user));
+    await this.setState({
+      loading: true,
+    })
+    await sleep(2000);
+    // await AsyncStorage.setItem('token', 'token');
+    // const user = {user: 'user'}
+    // await AsyncStorage.setItem('user', JSON.stringify(user));
     const { dispatch } = this.props.navigation;
     dispatch(navigateToApp);
   }
+
   render() {
     return (
       <Layout
-        toolbarTitle="Login"
+        toolbarTitle="Login with Github"
       >
-        <Text>Login Screen</Text>
-        <Button
-          onPress={this.login}
-          title="Login"
+        <TextInput
+          placeholder="Username"
+          style={styles.textfield}
+          onChangeText={(username) => this.setState({username})}
+          value={this.state.username}
+          returnKeyType="next"
+          editable={!this.state.loading}
+          onSubmitEditing={() => this.passwordInput.focus()}
+          autoFocus
         />
+        <TextInput
+          placeholder="Password"
+          ref={(c) => this.passwordInput = c}
+          style={styles.textfield}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          editable={!this.state.loading}
+          onSubmitEditing={this.login}
+          returnKeyType="go"
+          secureTextEntry
+        />
+        <View style={styles.button}>
+          <Button
+            color={primary}
+            onPress={this.login}
+            title="Login"
+            disabled={!this.state.username || !this.state.password || this.state.loading}
+          />
+        </View>
       </Layout>
     )
   }
