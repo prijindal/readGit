69a5df4f695eb19a86c9c96d65dd093d58d0be02
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-07-22 22:47:58 +0530

    Added Tokens in build

---
+++ src/tokens.js
@@ -0,0 +1,16 @@
+export const CLIENT_ID = 'c6afa760610b0177b86b';
+export const CLIENT_SECRET = 'b40dc4d20753cb9ac2a9e8741ecf04574516f422';
+export const scopes = [
+  'user',
+  'user:email',
+  'user:follow',
+  'repo:status',
+  'repo',
+  'public_repo',
+  'notifications',
+  'gist',
+  'delete_repo',
+  'read:org'
+];
+
+export const GIT_APP_NAME = 'Read Git APP';
