ac5bd110a92932932930a32f85e32a7f5f5de5cd
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-07-22 23:21:34 +0530

    Fixed Build configuration

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-yarn run android:release
+# yarn run android:release
+
+rm -rf build
+
+mkdir build
 
 cp android/app/build/outputs/apk/app-release.apk build/
 
@@ -8,10 +12,14 @@ git init
 
 git remote add origin git@github.com:prijindal/readgit.git
 
+git config user.email "priyanshujindal1995@gmail.com"
+
+git config user.name "Priyanshu Jindal"
+
 git checkout --orphan build
 
 git add .
 
 git commit -am "BUILD: $(date)"
 
-git push -u origin build
+git push -u origin build --force
