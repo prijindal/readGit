0c11289ca00fc8f55d2c403c75999118a41128e9
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2017-07-27 15:08:05 +0530

    Added Black Colors

--- src/colors.js
+++ src/colors.js
@@ -3,25 +3,32 @@ import color from 'color';
 
 export const transparent = 'rgba(0, 0, 0, 0)';
 
-export const primary = '#9C27B0';
+export const primary = '#212121';
 
-export const primaryDark = '#7b1fa2';
-
-export const accent = '#b2ff59';
+export const primaryDark = '#000000';
+export const primaryLight = '#484848';
 
 export const errorColor = color('#FF1744').alpha(0.87).hsl();
 
 export const white = '#fff';
 
-export const textPrimary = color(white).alpha(1).hsl();
-export const textSecondary = color(white).alpha(0.7).hsl();
-export const textDivider = color(white).alpha(0.54).hsl();
-export const textDisabled = color(white).alpha(0.12).hsl();
+export const background = '#F5F5F6';
+
+export const textLightPrimary = color(white).alpha(1).hsl();
+export const textLightSecondary = color(white).alpha(0.7).hsl();
+export const textLightDivider = color(white).alpha(0.54).hsl();
+export const textLightDisabled = color(white).alpha(0.12).hsl();
 
 export const textDark = '#000000';
 export const textDarkPrimary = color(textDark).alpha(0.87).hsl();
 export const textDarkSecondary = color(textDark).alpha(0.54).hsl();
-export const textDarkDivider = color(textDark).alpha(0.12).hsl();;
+export const textDarkDivider = color(textDark).alpha(0.3).hsl();
+export const textDarkDisabled = color(textDark).alpha(0.12).hsl();;
+
+export const textPrimary = textLightPrimary;
+export const textSecondary = textLightSecondary;
+export const textDivider = textLightDivider;
+export const textDisabled = textLightDisabled;
 
 export const tabBarOptions = {
   activeTintColor: textPrimary,
@@ -35,10 +42,10 @@ export const tabBarOptions = {
     height: 48,
   },
   style: {
-    backgroundColor: primary
+    backgroundColor: primaryLight
   },
   indicatorStyle: {
-    backgroundColor: accent,
-    height: 2
+    backgroundColor: primaryDark,
+    height: 3
   }
 }
